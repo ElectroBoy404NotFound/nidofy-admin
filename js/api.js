25ebaddb8a0ee37fe9217f297db594a3f7623d8f
Nikunj Doke (RPi) <me@nikunjdoke.in>
2026-07-25 21:16:31 +0530

    Missing slash... sigh....

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const isLocalhost = location.port == 5500; // FIXME Only works for VS Code's dev server
-const API_BASE_URL = isLocalhost ? "https://nido.dino.icu/api/" : "/api";
+const API_BASE_URL = isLocalhost ? "https://nido.dino.icu/api/" : "/api/";
 // const API_BASE_URL = "http://localhost:8081/";
 
 async function sendPOSTRequest(url, body, authtoken) {
